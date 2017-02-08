15d384bf3b4ce219e0247ce0effa1543b9db28a3
christiancoogan <christian.coogan@gmail.com>
2017-02-07 21:10:16 -0800

    print date

--- src/module4/Module4.java
+++ src/module4/Module4.java
@@ -1,12 +1,12 @@
 package module4;
-
+import java.util.Date;
 /**
  * Created by Christian on 2/7/2017.
  */
 public class Module4 {
     public static void main(String[] args) {
         System.out.println("Test test test");
-
+        System.out.println(new Date());
     }
 
 
