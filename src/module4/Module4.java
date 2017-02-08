adfa573b88282f3353a2e1a530dd40f13fde7c12
christiancoogan <christian.coogan@gmail.com>
2017-02-07 21:06:41 -0800

    added main method that prints test test test

--- src/module4/Module4.java
+++ src/module4/Module4.java
@@ -4,4 +4,12 @@ package module4;
  * Created by Christian on 2/7/2017.
  */
 public class Module4 {
+    public static void main(String[] args) {
+        System.out.println("Test test test");
+
+    }
+
+
+
+
 }
